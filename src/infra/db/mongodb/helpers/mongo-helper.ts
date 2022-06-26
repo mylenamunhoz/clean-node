461d5f6b15a148bec2436c98e95662dc2a41cf65
Mylena Munhoz <mylena.prado@edu.unipar.br>
2022-06-26 10:57:46 -0300

    refactor: change function

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,16 +1,17 @@
+
 import { MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
+  uri: null as unknown as string,
 
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }) as any
+    this.uri = uri
+    this.client = await MongoClient.connect(uri)
   },
 
-  async disconnect () {
+  async disconnect (): Promise<void> {
     await this.client.close()
+    this.client = null as any
   }
 }
